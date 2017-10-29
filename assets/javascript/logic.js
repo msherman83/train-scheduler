b58e763cfb654f6c9240ea028d5a4cc90ee6c341
msherman83 <msherman83@gmail.com>
2017-10-29 18:10:21 -0400

    initial commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,17 @@
+// TO DO
+// Initialize firebase.
+// 
+
+
+// Firebase engaged 
+var config = {
+    apiKey: "AIzaSyCDz0vMX53PMPph105iFgcjaBFEZeRc2c0",
+    authDomain: "train-scheduler-d535a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d535a.firebaseio.com",
+    projectId: "train-scheduler-d535a",
+    storageBucket: "",
+    messagingSenderId: "781081358402"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
